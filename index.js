02c43e07a307c443bd5d1da00588913bb76135fb
EveOfMaly <john.a.kyeremeh@gmail.com>
2022-05-04 10:17:06 -0400

    commit tree 4579ed4880a9ac5e09b6c3a6cb811906288f609b

--- index.js
+++ index.js
@@ -1,1 +1,81 @@
 // Your code here
+
+function mapToNegativize (sourceArray) {
+   const arrayCopy = sourceArray.slice();
+    
+    arrayCopy.forEach((value, index) => { 
+         arrayCopy[index] = value * -1 
+    })
+    return arrayCopy;   
+}
+
+function mapToNoChange(sourceArray) {
+    const arrayCopy = sourceArray.slice();
+     
+     arrayCopy.forEach((value, index) => { 
+          arrayCopy[index] = value 
+     })
+     return arrayCopy;   
+ }
+
+function mapToDouble(sourceArray) {
+    const arrayCopy = sourceArray.slice();
+     
+     arrayCopy.forEach((value, index) => { 
+          arrayCopy[index] = value * 2
+     })
+     return arrayCopy;   
+ }
+
+ function mapToSquare(sourceArray) {
+    const arrayCopy = sourceArray.slice();
+     
+     arrayCopy.forEach((value, index) => { 
+          arrayCopy[index] = value ** 2
+     })
+     return arrayCopy;   
+ }
+
+
+ function reduceToTotal(sourceArray, startingPoint = 0 ) {
+     sourceArray.forEach((value) => {
+    
+         startingPoint = value + startingPoint
+     })
+     return startingPoint;   
+ }
+
+
+ sourceArray = [ false, null, null, null, true]
+
+ function reduceToAllTrue(sourceArray) {
+     let truePresent = false
+     let falseCounter = 0
+
+     sourceArray.forEach((value, index) => { 
+         sourceArray[index] === false ? falseCounter++  : falseCounter
+     })
+
+     if (falseCounter == 0) {
+        truePresent = true
+     }
+
+    return truePresent
+ }
+
+ function reduceToAnyTrue(sourceArray) {
+    let truePresent = false
+
+    sourceArray.forEach((value, index) => { 
+        sourceArray[index] === true ? truePresent = true : truePresent = false
+    })
+   return truePresent
+}
+
+
+
+
+    
+
+
+    
